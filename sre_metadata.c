955481ee707b38a19aa621d11d90bb7c8b52aa4a
Nanqinqin Li <linanqinqin@princeton.edu>
2025-03-05 18:35:53 -0500

    change spinlock name

--- sre_metadata.c
+++ sre_metadata.c
@@ -3,7 +3,7 @@
 
 // Define hash table and spinlock
 DEFINE_HASHTABLE(sre_metadata_hash, SRE_HASH_BITS);
-DEFINE_SPINLOCK(sre_hash_lock);
+DEFINE_SPINLOCK(sre_flags_spinlock);
 
 // Add a GPA to the hash table
 void sre_flags_new(gpa_t gpa) {
@@ -13,11 +13,11 @@ void sre_flags_new(gpa_t gpa) {
     entry->gpa = gpa;
     entry->is_sre = false;          // is_sre should be initialized to false; the flash emulation layer sets it later 
     entry->is_ept = true;           // is_ept should be initialized to true, since the first occurence is always a regular EPT violation 
-    atomic_set(&entry->access_count, 0);
+    atomic_set(&entry->access_count, 0);    // probably does not need atomic here at init
 
-    spin_lock(&sre_hash_lock);
+    spin_lock(&sre_flags_spinlock);
     hash_add(sre_metadata_hash, &entry->node, gpa);
-    spin_unlock(&sre_hash_lock);
+    spin_unlock(&sre_flags_spinlock);
 }
 
 // Remove a GPA from the hash table
@@ -25,9 +25,9 @@ void sre_flgas_remove(gpa_t gpa) {
     struct sre_flags *entry;
     hash_for_each_possible(sre_metadata_hash, entry, node, gpa) {
         if (entry->gpa == gpa) {
-            spin_lock(&sre_hash_lock);
+            spin_lock(&sre_flags_spinlock);
             hash_del(&entry->node);
-            spin_unlock(&sre_hash_lock);
+            spin_unlock(&sre_flags_spinlock);
             kfree(entry);
             break;
         }
