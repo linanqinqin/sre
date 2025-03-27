2d5fd585bcf976a24e0b89089a60950234f23056
Nanqinqin Li <33946891+linanqinqin@users.noreply.github.com>
2025-03-27 15:33:17 -0400

    start_vm.sh: change img path

--- start_vm.sh
+++ start_vm.sh
@@ -8,7 +8,7 @@ sudo qemu-system-x86_64 \
     -m 4G \
     -device virtio-scsi-pci,id=scsi0 \
     -device scsi-hd,drive=hd0,id=disk0 \
-    -drive file=/mnt/u20s.qcow2,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
+    -drive file=/opt/u20s.qcow2,if=none,aio=native,cache=none,format=qcow2,id=hd0 \
     -net user,hostfwd=tcp::8080-:22 \
     -net nic,model=virtio \
     -daemonize \
